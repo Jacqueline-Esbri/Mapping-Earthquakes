46bdd33443d3e065b9542915776ce5fa1e621ff4
je8778 <jackie6578@gmail.com>
2021-05-14 20:44:01 -0400

    editing logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -53,60 +53,6 @@ console.log("working");
 
 // }).addTo(map);
 
-
-
-// Coordinates for each point to be used in the line.
-// let line = [
-//   [33.9416, -118.4085],
-//   [37.6213, -122.3790],
-//   [40.7899, -111.9791],
-//   [47.4502, -122.3088]
-// ];
-
-// Create a polyline using the line coordinates and make the line red.
-// L.polyline(line, {
-//   color: "yellow"
-// }).addTo(map);
-
-
-// Add a circle marker to Los Angeles, CA
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//  }).addTo(map);
-
-// Circle (light-yellow) w blk lines 300-px on drk map
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "black",
-//     fillColor: '#ffffa1'
-// }).addTo(map);
-    
-
-//  Add a marker to the map for Los Angeles, California.
-//   let marker = marker([34.0522, -118.2437]).addTo(map);
-
-// Get data from cities.js
-// let cityData = cities;
-
-  // Loop through the cities array and create one marker for each city. (marker)
-//   cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location)
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
-  
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location, {
-//     radius: city.population/100000
-// })
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
-
-
-
 // OPTION #1 
 // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
